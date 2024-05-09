117f2f7ff0d1e1bb608ef34438b15ee4b652c7d4
12Ahn22 <12ayo22@gmail.com>
2024-05-09 17:53:17 +0900

    [5/9] 하던거 미리 저장

---
+++ 코딩테스트합격자되기/스택/진수변환/Main.java
@@ -0,0 +1,14 @@
+package 코딩테스트합격자되기.스택.진수변환;
+
+public class Main {
+    public void solution() {
+
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        System.out.println(T.solution(10));
+        System.out.println(T.solution(27));
+        System.out.println(T.solution(10));
+    }
+}
